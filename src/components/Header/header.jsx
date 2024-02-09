50e793b3fc8a91e0622e18583163adaddf46ce15
ArtemChubur <artemchubur@gmail.com>
2024-02-09 20:08:21 +0600

    fix footer hover and update modal windov

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,43 +1,52 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import CloseIcon from '@mui/icons-material/Close';
-import logo from "../../assets/logo.png"
-import {useLocation, useParams} from "react-router-dom";
-import { motion } from "framer-motion";
-import {container} from "../../constants/animate";
-import Main from "../../Pages/Main";
-import photochka from '../../assets/photochka_city.jpg'
-import './header.css'
 import axios from "axios";
+import { useLocation, useParams } from "react-router-dom";
+import CircularProgress from '@mui/material/CircularProgress';
+import { motion } from "framer-motion";
+import { container } from "../../constants/animate";
+import logo from "../../assets/logo.png"
+import './header.css'
 
-export const Header = ({page}) => {
+export const Header = ({ page }) => {
     const [activePage, setActivePage] = useState(false);
     const route = useLocation()
     const [activeModalWindow, setActiveModalWindow] = useState(false);
     const [ticketError, setTicketError] = useState(false);
+    const [loader, setLoader] = useState(false)
+    const [ticketMessages, setTicketMessages] = useState('')
 
     useEffect(() => {
         if (route.pathname === '/about') {
             setActivePage(true)
         } else {
             setActivePage(false)
-        }}, [])
+        }
+    }, [])
 
     const [ticket, setTicket] = useState({
         username: '',
         phone: ''
-      });
+    });
 
     async function sendTicket() {
+        setLoader(true)
         try {
             const response = await axios.post('http://localhost:3333/send-message', ticket);
-            console.log(response.data);
-          } catch (error) {
+            // setTicketMessages('Ваша заявку успешно отправлена!')
+            if (response.status === 200) {
+                setTicketMessages('Заявка успешно отправлена!')
+            }
+        } catch (error) {
             console.error('Ошибка при отправке данных:', error);
-          }
+            setTicketMessages('Ошибка, попробуйте позже.')
+        } finally{
+            setLoader(false)
+        }
     }
 
     return (
-        <header>
+        <div>
             {activeModalWindow &&
                 <div className={`header-modal-window`}>
                     <motion.div
@@ -51,7 +60,9 @@ export const Header = ({page}) => {
                                 setActiveModalWindow(false)
                                 setTicketError(false)
                             }}
-                            className={'header-close-btn'}><CloseIcon /></button>
+                            className={'header-close-btn'}>
+                                <CloseIcon />
+                            </button>
                         {ticketError ?
                             <motion.div
                                 className='text_in_the_alert'
@@ -59,8 +70,9 @@ export const Header = ({page}) => {
                                 initial="hidden"
                                 whileInView="visible"
                             >
-                                <p className={'modalWindowError'}>Ваша заявка не была отправлена, так как нам не
-                                    предоставили backend для этого.</p>
+                                <p className={'modalWindowError'}>
+                                    {ticketMessages}
+                                </p>
                                 <div className={'bibiziankyParents'}>
                                     <img
                                         className='bibizianky'
@@ -69,35 +81,44 @@ export const Header = ({page}) => {
                                 </div>
                             </motion.div>
                             :
-                            <div  className='form_back-back'>
-                                <form onSubmit={null} className='formochki'>
-                                    <input onChange={(e) => setTicket(prevState => ({...prevState, username: e.target.value,}))} placeholder={'Ваше имя'} type="text"/>
-                                    <input onChange={(e) => setTicket(prevState => ({...prevState, phone: e.target.value,}))} pattern='0[0-9]{3}[0-9]{3}[0-9]{3}' type="tel" placeholder={'Номер телефона'}/>
-                                    {/* <button onClick={() => {setTicketError(true)}} type="button">Отправить</button> */}
-                                    <button onClick={sendTicket} type="button">Отправить</button>
-                            </form>
-                        </div>
+                            <div className='form_back-back'>
+                                {loader ?
+                                    <div>
+                                        <CircularProgress />
+                                    </div>
+                                    :
+                                    <form className='formochki'>
+                                        <input onChange={(e) => setTicket(prevState => ({ ...prevState, username: e.target.value, }))} placeholder={'Ваше имя'} type="text" />
+                                        <input onChange={(e) => setTicket(prevState => ({ ...prevState, phone: e.target.value, }))} pattern='0[0-9]{3}[0-9]{3}[0-9]{3}' type="tel" placeholder={'Номер телефона'} />
+                                        <button onClick={() => {
+                                            sendTicket()
+                                            setTicketError(true)
+                                        }} type="button">Отправить</button>
+                                    </form>
+                                }
+                            </div>
                         }
                     </motion.div>
                 </div>
             }
             <header className="header">
                 <div className={'header-left'}>
-                    <a href="../"><img src={logo} alt=""/></a>
+                    <a href="../"><img src={logo} alt="" /></a>
                 </div>
                 <div className="header-right">
                     <a
                         href="../"
-                       className={`header-link ${activePage || 'header-link-activ'}`}>
-                    Главная</a>
+                        className={`header-link ${activePage || 'header-link-activ'}`}>
+                        Главная</a>
                     <a
                         href={'/about'}
                         className={`header-link ${activePage && 'header-link-activ'}`}>
-                    О нас</a>
+                        О нас</a>
 
-                    <button onClick={() => {setActiveModalWindow(true)}} className="header-button">+Добавить обьявление</button>
+                    <button onClick={() => { setActiveModalWindow(true) }} className="header-button">+Добавить обьявление</button>
                 </div>
             </header>
-        </header>
+        </div>
     )
 }
+export default Header;
--- src/constants/partners.js
+++ src/constants/partners.js
@@ -4,4 +4,23 @@ import partner3 from '../assets/PejBEGRRHr-removebg-preview (1) 1.png'
 import partner4 from '../assets/qGcJsIZ3cy-removebg-preview (1) 3.png'
 import partner5 from '../assets/6L21Bmus4K-removebg-preview (1) 2.png'
 
-export const partners = [partner1, partner2, partner3, partner4, partner5]
+export const partners = [{
+    logo: partner1,
+    link: '',
+}, 
+{
+    logo: partner2,
+    link: '',
+}, 
+{
+    logo: partner3,
+    link: '',
+},
+{
+    logo: partner4,
+    link: '',
+}, 
+{
+    logo: partner5,
+    link: '',
+}]
