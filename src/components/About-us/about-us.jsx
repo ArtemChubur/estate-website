579d603a1b32462f0fdae7137d03496c556313b9
ArtemChubur <artemchubur@gmail.com>
2024-02-09 15:13:12 +0600

    add verstca for about page

--- src/components/About-us/about-us.jsx
+++ src/components/About-us/about-us.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
 import "./about-us.css"
+import { advantages_left, advantages_right } from '../../constants/advantages';
 
 const AboutUs = () => {
     return (
-        <div>
-
-        </div>
+        <section>
+            <h2>Property Featured</h2>
+            <p>Real estate is a crowded field. But with the number of buyers purchasing homes through real estate agents and brokers growing there’s plenty of </p>
+            <div>
+                {advantages_left.map((item, idx) => {
+                    return (
+                        <div>
+                            <img src={item.src} alt="" />
+                            <h3>{item.title}</h3>
+                            <p>{item.text}</p>
+                        </div>
+                    )
+                })}
+            </div>
+            <div></div>
+            <div>
+                {advantages_right.map((item, idx) => {
+                    return (
+                        <div>
+                            <img src={item.src} alt="" />
+                            <h3>{item.title}</h3>
+                            <p>{item.text}</p>
+                        </div>
+                    )
+                })}
+            </div>
+        </section>
     );
 };
 
---
+++ src/constants/advantages.js
@@ -0,0 +1,53 @@
+import advantages_security from "../assets/advantages-security.png";
+import advantages_conditioner from "../assets/advantages-conditioner.png";
+import advantages_garden from "../assets/advantages-garden.png";
+
+import advantages_parkers from "../assets/advantages-parkers.png";
+import advantages_furniture from "../assets/advantages-furniture.png";
+import advantages_pool from "../assets/advantages-pool.png";
+
+
+
+const advantages_left = [
+    {
+        id: 0,
+        title: '100% Security',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_security
+    },
+    {
+        id: 1,
+        title: 'Free air conditioner',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_conditioner
+    },
+    {
+        id: 2,
+        title: 'Flower garden',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_garden
+    }
+]
+
+const advantages_right = [
+    {
+        id: 0,
+        title: 'Parkers & movers',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_parkers
+    },
+    {
+        id: 1,
+        title: 'Rental furniture',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_furniture
+    },
+    {
+        id: 2,
+        title: 'Swimming pool',
+        text: 'For many of us, buying a home is a dre and one not easily attainable.',
+        src: advantages_pool
+    }
+]
+
+export {advantages_left, advantages_right}
